90dcbbefe1df547df9de96f7c9ed8c02ccdd421b
ros <burhanbhos1997@gmail.com>
2020-02-25 16:26:46 +0100

    robot_hardware_interface.h and robot_hardware_interface_node.cpp are orginized

--- src/mrm_description/include/mrm_description/robot_hardware_interface.h
+++ src/mrm_description/include/mrm_description/robot_hardware_interface.h
@@ -3,54 +3,25 @@
 #include <hardware_interface/robot_hw.h>
 #include <controller_manager/controller_manager.h>
 #include <ros/ros.h>
+#include <boost/scoped_ptr.hpp>
 
 class MyRobot : public hardware_interface::RobotHW
 {
 public:
-  MyRobot() 
- { 
-   // connect and register the joint state interface
-   hardware_interface::JointStateHandle state_handle_base_waist("base_waist", &pos[0], &vel[0], &eff[0]);
-   jnt_state_interface.registerHandle(state_handle_base_waist);
-
-   hardware_interface::JointStateHandle state_handle_waist_forearm("waist_forearm", &pos[1], &vel[1], &eff[1]);
-   jnt_state_interface.registerHandle(state_handle_waist_forearm);
-
-    hardware_interface::JointStateHandle state_handle_forearm_forearmRotation("forearm_forearmRotation", &pos[2], &vel[2], &eff[2]);
-   jnt_state_interface.registerHandle(state_handle_forearm_forearmRotation);
-
-   hardware_interface::JointStateHandle state_handle_forearmRotation_wrist("forearmRotation_wrist", &pos[3], &vel[3], &eff[3]);
-   jnt_state_interface.registerHandle(state_handle_forearmRotation_wrist);
-
-   hardware_interface::JointStateHandle state_handle_wrist_wristRotation("wrist_wristRotation", &pos[4], &vel[4], &eff[4]);
-   jnt_state_interface.registerHandle(state_handle_wrist_wristRotation);
-
-   registerInterface(&jnt_state_interface);
-
-   // connect and register the joint position interface
-   hardware_interface::JointHandle pos_handle_base_waist(jnt_state_interface.getHandle("base_waist"), &cmd[0]);
-   jnt_pos_interface.registerHandle(pos_handle_base_waist);
-
-   hardware_interface::JointHandle pos_handle_waist_forearm(jnt_state_interface.getHandle("waist_forearm"), &cmd[1]);
-   jnt_pos_interface.registerHandle(pos_handle_waist_forearm);
-
-   hardware_interface::JointHandle pos_handle_forearm_forearmRotation(jnt_state_interface.getHandle("forearm_forearmRotation"), &cmd[2]);
-   jnt_pos_interface.registerHandle(pos_handle_forearm_forearmRotation);
-
-   hardware_interface::JointHandle pos_handle_forearmRotation_wrist(jnt_state_interface.getHandle("forearmRotation_wrist"), &cmd[3]);
-   jnt_pos_interface.registerHandle(pos_handle_forearmRotation_wrist);
-
-   hardware_interface::JointHandle pos_handle_wrist_wristRotation(jnt_state_interface.getHandle("wrist_wristRotation"), &cmd[4]);
-   jnt_pos_interface.registerHandle(pos_handle_wrist_wristRotation);
-
-   registerInterface(&jnt_pos_interface);
-  }
+  MyRobot(ros::NodeHandle& nh);
+  void init();
 
 private:
   hardware_interface::JointStateInterface jnt_state_interface;
   hardware_interface::PositionJointInterface jnt_pos_interface;
+
+  int num_joints_;
+  std::string joint_names_[5]; 
   double cmd[5];
   double pos[5];
   double vel[5];
   double eff[5];
+
+  ros::NodeHandle nh_;
+  boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
 };
--- src/mrm_description/src/robot_hardware_interface_node.cpp
+++ src/mrm_description/src/robot_hardware_interface_node.cpp
@@ -1,12 +1,40 @@
 #include <mrm_description/robot_hardware_interface.h>
 
+MyRobot::MyRobot(ros::NodeHandle& nh) : nh_(nh){
+    init();
+    controller_manager_.reset(new controller_manager::ControllerManager(this, nh_));
+}
+
+void MyRobot::init(){
+    num_joints_ = 5;
+    joint_names_[0] = "base_waist";
+    joint_names_[1] = "waist_forearm";
+    joint_names_[2] = "forearm_forearmRotation";
+    joint_names_[3] = "forearmRotation_wrist";
+    joint_names_[4] = "wrist_wristRotation";
+
+    for (int i = 0; i < num_joints_; i++)
+    {
+        hardware_interface::JointStateHandle joint_state_handle(joint_names_[i], &pos[0], &vel[0], &eff[0]);
+        jnt_state_interface.registerHandle(joint_state_handle);
+
+        hardware_interface::JointHandle joint_pos_handle(jnt_state_interface.getHandle(joint_names_[i]), &cmd[0]);
+        jnt_pos_interface.registerHandle(joint_pos_handle);
+    }
+
+    registerInterface(&jnt_state_interface);
+    registerInterface(&jnt_pos_interface);
+    
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "robot_hardware_interface");
+    ros::NodeHandle nh;
     //ros::AsyncSpinner spinner(2);
     //spinner.start();
-    MyRobot robot;
-    controller_manager::ControllerManager cm(&robot);
+    MyRobot robot(nh);
+    //controller_manager::ControllerManager cm(&robot);
     //ros::spin();
 
     while(1);
